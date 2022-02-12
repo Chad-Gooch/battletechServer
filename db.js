cf20479694418ab80deed972e706a99865ab4c02
Chad-Gooch <chad.t.gooch88@gmail.com>
2022-02-12 09:59:43 -0500

    connect

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+//const sequelize = new Sequelize(`${DATABASE_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 
-/*
+
 sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
-        require: false,
+        require: true,
         rejectUnauthorized: false
       }
     }
   }
 );
-*/
+
 
 module.exports = sequelize;
