c7ff767444c40ad9513e6136c4627a8cc7345b55
Chad-Gooch <chad.t.gooch88@gmail.com>
2022-02-12 12:11:20 -0500

    test

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);
-
-/*
 sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
+}
 );
-*/
+
+/*const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);*/
+
 
 module.exports = sequelize;
